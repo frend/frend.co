23d81bd811e57ae82f55dff398495bdf4145a806
Thomas Digby <thomasdigby1@gmail.com>
2016-01-10 14:39:25 +0000

    Refactored to updated design pattern

--- _components/offcanvas/offcanvas.js
+++ _components/offcanvas/offcanvas.js
@@ -1,99 +1,152 @@
 'use strict';
 
-
-let elemSelector = {};
-let elemToggle = {};
-const namespace = 'froffcanvas';
-
-
-let Froffcanvas = function(selector = '.js-fr-offcanvas', toggle = '.js-fr-offcanvas-toggle') {
-
-
-	elemSelector = document.querySelector(selector);
-	elemToggle = document.querySelector(toggle);
-
-
-	function init() {
-		bindPointer();
-	}
+/**
+ * @param {string} panelSelector 	Off-canvas element
+ * @param {string} toggleSelector 	Button with which to toggle the off-canvas state
+ * @param {object} options			Object containing config overrides
+ */
+const Froffcanvas = function(
+		panelSelector = '.js-fr-offcanvas',
+		toggleSelector = '.js-fr-offcanvas-toggle',
+		{
+			activePanelClass: activePanelClass = 'fr-offcanvas--is-active',
+			readyClass: readyClass = 'has-fr-offcanvas'
+		} = {}
+	) {
 
 
-	//	Add event bindings
-	function bindPointer() {
-		elemToggle.addEventListener('click', eventPointer);
-	}
-	function bindDocumentKey() {
-		document.addEventListener('keyup', eventDocumentKey);
-	}
-	function bindDocumentClick() {
-		document.addEventListener('click', eventDocumentClick);
-	}
+	//	CONSTANTS
+	const doc = document;
+	const docEl = doc.documentElement;
 
 
-	//	Remove event bindings
-	function unbindDocumentKey() {
-		document.removeEventListener('keyup', eventDocumentKey);
-	}
-	function unbindDocumentClick() {
-		document.removeEventListener('click', eventDocumentClick);
-	}
+	//	SUPPORTS
+	if (!'querySelector' in document || !'addEventListener' in window || !docEl.classList) return;
 
 
-	//	Event handlers
-	function eventPointer() {
-		if (elemSelector.getAttribute('aria-hidden') == 'false') {
-			hideOffcanvas();
-		} else {
-			showOffcanvas();
-		}
-	}
-	function eventDocumentKey(e) {
-		//	esc key
-		if (e.keyCode === 27) hideOffcanvas();
-	}
-	function eventDocumentClick(e) {
-		//	check if target is offcanvas or child of
-		let isOffcanvas = e.target == elemSelector;
-		let childOfOffcanvas = checkParent(e.target, elemSelector);
-		if (!isOffcanvas && !childOfOffcanvas) hideOffcanvas();
-	}
+	//	SETUP
+	let panel = doc.querySelector(panelSelector);
+	let toggle = doc.querySelector(toggleSelector);
 
 
-	//	Toggle component
-	function showOffcanvas() {
-		//	undo aria-hidden, add focus
-		elemSelector.setAttribute('aria-hidden', false);
-		elemSelector.setAttribute('tabindex', 0);
-		elemSelector.focus();
-		//	bind document close events
+	//	UTILS
+	function _defer (func) {
 		//	wrapped in setTimeout to delay binding until previous rendering has completed
-		setTimeout(bindDocumentKey, 0); // this isn't working for enter, works for space though. WTF.
-		setTimeout(bindDocumentClick, 0);
+		if (typeof func === 'function') setTimeout(func, 0);
 	}
-	function hideOffcanvas() {
-		//	set aria-hidden, remove focus
-		elemSelector.setAttribute('aria-hidden', true);
-		elemSelector.setAttribute('tabindex', -1);
-		elemSelector.blur();
-		//	unbind document close events
-		unbindDocumentKey();
-		unbindDocumentClick();
+	function _closest (el, fn) {
+		// closest: http://clubmate.fi/jquerys-closest-function-and-pure-javascript-alternatives/
+		return el && (fn(el) ? el : _closest(el.parentNode, fn));
 	}
 
 
-	//	DOM utils
-	function checkParent(elem, parent) {
-		let parentNode = elem.parentNode;
-		while (parentNode != null) {
-			if (parentNode == parent) return true;
-			parentNode = parentNode.parentNode;
+	//	A11y
+	function _addA11y () {
+		//	add aria-hidden attribute
+		panel.setAttribute('aria-hidden', true);
+	}
+	function _removeA11y () {
+		//	add aria-hidden attribute
+		panel.removeAttribute('aria-hidden');
+	}
+
+
+	//	Events
+	function _eventPointer () {
+		let panelHidden = panel.getAttribute('aria-hidden') === 'true';
+		if (panelHidden) {
+			_showPanel();
+		} else {
+			_hidePanel();
 		}
-		return false;
+	}
+	function _eventDocClick (e) {
+		//	check if target is panel or child of
+		let isPanel = e.target == panel;
+		let isPanelChild = _closest(e.target, (el) => {
+			if (el != doc) return el.classList.contains(panelSelector.substring(1));
+		});
+		if (!isPanel && !isPanelChild) _hidePanel();
+	}
+	function _eventDocKey (e) {
+		//	esc key
+		if (e.keyCode === 27) _hidePanel();
 	}
 
 
-	//	Run
-	if (elemSelector) init();
+	//	Bindings
+	function _bindPointer () {
+		toggle.addEventListener('click', _eventPointer);
+	}
+	function _bindDocClick () {
+		doc.addEventListener('click', _eventDocClick);
+	}
+	function _bindDocKey () {
+		doc.addEventListener('keydown', _eventDocKey);
+	}
+
+	//	Unbind
+	function _unbindPointer () {
+		toggle.removeEventListener('click', _eventPointer);
+	}
+	function _unbindDocClick () {
+		doc.removeEventListener('click', _eventDocClick);
+	}
+	function _unbindDocKey () {
+		doc.removeEventListener('keydown', _eventDocKey);
+	}
+
+
+	//	Actions
+	function _showPanel () {
+		//	remove aria-hidden, add focus
+		panel.setAttribute('aria-hidden', false);
+		panel.setAttribute('tabindex', 0);
+		panel.focus();
+		//	bind document close events
+		_defer(_bindDocClick); // this isn't working for enter, works for space though. WTF.
+		_defer(_bindDocKey);
+		//	add active class
+		panel.classList.add(activePanelClass);
+	}
+	function _hidePanel () {
+		//	add aria-hidden, remove focus
+		panel.setAttribute('aria-hidden', true);
+		panel.setAttribute('tabindex', -1);
+		panel.blur();
+		//	unbind document events
+		_unbindDocKey();
+		_unbindDocClick();
+		//	remove active class
+		panel.classList.remove(activePanelClass);
+	}
+	function destroy () {
+		//	remove attributes
+		_removeA11y();
+		//	unbind events
+		_unbindPointer();
+		_unbindDocClick();
+		_unbindDocKey();
+		//	remove reference
+		docEl.classList.remove(readyClass);
+	}
+
+
+	//	INIT
+	function _init () {
+		if (panel) {
+			_addA11y();
+			_bindPointer();
+			docEl.classList.add(readyClass);
+		}
+	}
+	_init();
+
+
+	// REVEAL API
+	return {
+		destroy
+	}
 }
 
 
