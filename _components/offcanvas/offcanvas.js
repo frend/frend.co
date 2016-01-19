b6d6b5d14cfec3301efa1fd1911231e4289541c7
Thomas Digby <thomasdigby1@gmail.com>
2016-01-19 21:26:01 +0000

    Added a bunch of stuff as per recent tickets.

--- _components/offcanvas/offcanvas.js
+++ _components/offcanvas/offcanvas.js
@@ -5,12 +5,15 @@
  * @param {string} toggleSelector 	Button with which to toggle the off-canvas state
  * @param {object} options			Object containing config overrides
  */
-const Froffcanvas = function(
-		panelSelector = '.js-fr-offcanvas',
-		toggleSelector = '.js-fr-offcanvas-toggle',
+const FrOffcanvas = function(
+		selector = '.js-fr-offcanvas',
 		{
-			activePanelClass: activePanelClass = 'fr-offcanvas--is-active',
-			readyClass: readyClass = 'has-fr-offcanvas'
+			openSelector = openSelector = '.js-fr-offcanvas-open',
+			closeSelector = closeSelector = '.js-fr-offcanvas-close',
+			toggleSelector = toggleSelector = '.js-fr-offcanvas-toggle',
+			readyClass: readyClass = 'has-fr-offcanvas',
+			activeClass: activeClass = 'fr-offcanvas-is-active',
+			panelActiveClass: panelActiveClass = 'fr-offcanvas--is-active'
 		} = {}
 	) {
 
@@ -18,7 +21,6 @@ const Froffcanvas = function(
 	//	CONSTANTS
 	const doc = document;
 	const docEl = doc.documentElement;
-	// const html = doc.getElementByTagName('html');
 	const transitionEventSyntax = {
 		transition: 'transitionend',
 		WebkitTransition: 'webkitTransitionEnd',
@@ -28,14 +30,15 @@ const Froffcanvas = function(
 
 
 	//	SUPPORTS
-	if (!'querySelector' in document || !'addEventListener' in window || !docEl.classList) return;
+	if (!'querySelector' in doc || !'addEventListener' in window || !docEl.classList) return;
 
 
 	//	SETUP
-	let panel = doc.querySelector(panelSelector);
-	let toggle = doc.querySelector(toggleSelector);
+	let panel = doc.querySelector(selector);
+	let buttonOpen = doc.querySelector(openSelector);
+	let buttonClose = doc.querySelector(closeSelector);
+	let buttonToggle = doc.querySelector(toggleSelector);
 	let transitionEventName = 'transitionend';
-	let docOverflow = {};
 
 
 	//	UTILS
@@ -47,10 +50,6 @@ const Froffcanvas = function(
 		// closest: http://clubmate.fi/jquerys-closest-function-and-pure-javascript-alternatives/
 		return el && (fn(el) ? el : _closest(el.parentNode, fn));
 	}
-	function _getPropValue (el, prop) {
-		//	return css property value
-		return doc.defaultView.getComputedStyle(el, null).getPropertyValue(prop);
-	}
 
 
 	//	Cross-browser
@@ -59,11 +58,6 @@ const Froffcanvas = function(
 		for (var prefix in transitionEventSyntax) {
 			if (panel.style[prefix] !== undefined) return transitionEventSyntax[prefix];
 		}
-	}
-	function _setDocOverflowProperty () {
-		//	save document overflow properties to apply when closing panel
-		docOverflow.overflow = _getPropValue(docEl, 'overflow');
-		docOverflow.overflowY = _getPropValue(docEl, 'overflow-y');
 	}
 
 
@@ -79,7 +73,7 @@ const Froffcanvas = function(
 
 
 	//	Events
-	function _eventPointer () {
+	function _eventTogglePointer () {
 		let panelHidden = panel.getAttribute('aria-hidden') === 'true';
 		if (panelHidden) {
 			_showPanel();
@@ -87,11 +81,17 @@ const Froffcanvas = function(
 			_hidePanel();
 		}
 	}
+	function _eventOpenPointer () {
+		_showPanel();
+	}
+	function _eventClosePointer () {
+		_hidePanel();
+	}
 	function _eventDocClick (e) {
 		//	check if target is panel or child of
 		let isPanel = e.target == panel;
 		let isPanelChild = _closest(e.target, (el) => {
-			if (el != doc) return el.classList.contains(panelSelector.substring(1));
+			if (el != doc) return el.classList.contains(selector.substring(1));
 		});
 		if (!isPanel && !isPanelChild) _hidePanel();
 	}
@@ -108,8 +108,14 @@ const Froffcanvas = function(
 
 
 	//	Bindings
-	function _bindPointer () {
-		toggle.addEventListener('click', _eventPointer);
+	function _bindTogglePointer () {
+		if (!!buttonToggle) buttonToggle.addEventListener('click', _eventTogglePointer);
+	}
+	function _bindOpenPointer () {
+		if (!!buttonOpen) buttonOpen.addEventListener('click', _eventOpenPointer);
+	}
+	function _bindClosePointer () {
+		if (!!buttonClose) buttonClose.addEventListener('click', _eventClosePointer);
 	}
 	function _bindDocClick () {
 		doc.addEventListener('click', _eventDocClick);
@@ -123,7 +129,7 @@ const Froffcanvas = function(
 
 	//	Unbind
 	function _unbindPointer () {
-		toggle.removeEventListener('click', _eventPointer);
+		buttonToggle.removeEventListener('click', _eventPointer);
 	}
 	function _unbindDocClick () {
 		doc.removeEventListener('click', _eventDocClick);
@@ -138,24 +144,25 @@ const Froffcanvas = function(
 
 	//	Actions
 	function _showPanel () {
-		//	remove aria-hidden, add focus
-		panel.setAttribute('aria-hidden', false);
-		panel.setAttribute('tabindex', 0);
-		panel.focus();
 		//	set visibility to override any previous set style
 		panel.style.visibility = 'visible';
+		//	remove aria-hidden, add focus
+		panel.setAttribute('aria-hidden', false);
+		panel.setAttribute('tabindex', -1);
+		panel.focus();
 		//	bind document close events
 		_defer(_bindDocClick); // this isn't working for enter, works for space though. WTF.
 		_defer(_bindDocKey);
+		//	reset scroll position
+		panel.scrollTop = 0;
 		//	add active class
-		panel.classList.add(activePanelClass);
-		//	fix overflow to remove scroll when covering the entire viewport
-		_hideDocOverflow();
+		panel.classList.add(panelActiveClass);
+		docEl.classList.add(activeClass);
 	}
 	function _hidePanel () {
 		//	add aria-hidden, remove focus
 		panel.setAttribute('aria-hidden', true);
-		panel.setAttribute('tabindex', -1);
+		panel.removeAttribute('tabindex');
 		panel.blur();
 		//	bind transition end
 		_bindTransitionEnd();
@@ -163,9 +170,8 @@ const Froffcanvas = function(
 		_unbindDocKey();
 		_unbindDocClick();
 		//	remove active class
-		panel.classList.remove(activePanelClass);
-		//	fix overflow to remove scroll when covering the entire viewport
-		_resetDocOverflow();
+		panel.classList.remove(panelActiveClass);
+		docEl.classList.remove(activeClass);
 	}
 	function destroy () {
 		//	remove attributes
@@ -177,37 +183,33 @@ const Froffcanvas = function(
 		//	remove reference
 		docEl.classList.remove(readyClass);
 	}
-	function _resetDocOverflow () {
-		//	loop through overflow properties and set value
-		for (var prop in docOverflow) {
-			docEl.style[prop] = docOverflow[prop];
-		}
-	}
-	function _hideDocOverflow () {
-		//	prevent scrolling document when panel open
-		docEl.style.overflow = 'hidden';
-	}
 
 
 	//	INIT
-	function _init () {
+	function init () {
 		if (panel) {
-			_addA11y();
-			_bindPointer();
+			//	detect required properties
 			_setTransitionEventPrefix();
-			_setDocOverflowProperty();
+			//	set a11y DOM properties
+			_addA11y();
+			//	bind button events
+			_bindTogglePointer();
+			_bindOpenPointer();
+			_bindClosePointer();
+			//	set ready class
 			docEl.classList.add(readyClass);
 		}
 	}
-	_init();
+	init();
 
 
 	// REVEAL API
 	return {
+		init,
 		destroy
 	}
 }
 
 
 // module exports
-export default Froffcanvas;
+export default FrOffcanvas;
