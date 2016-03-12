2deb83f58abeca6c56550ed07c84e7e45371de3d
Thomas Digby <thomasdigby1@gmail.com>
2016-03-12 12:22:06 +0000

    Initial work

--- _components/dialogmodal/README.md
+++ _components/dialogmodal/README.md
@@ -1,14 +1,15 @@
 ---
-filename: dialog-modal
+permalink: /components/dialogmodal/
+filename: dialogmodal
 title: Dialog Modal
-alpha: true
+alpha: false
 sources:
   - title: HTML
-    url: https://github.com/frend/frend.co/blob/gh-pages/_components/dialog-modal/dialog-modal.html
+    url: https://github.com/frend/frend.co/blob/gh-pages/_components/dialogmodal/dialogmodal.html
   - title: CSS
-    url: https://github.com/frend/frend.co/blob/gh-pages/_components/dialog-modal/dialog-modal.css
+    url: https://github.com/frend/frend.co/blob/gh-pages/_components/dialogmodal/dialogmodal.css
   - title: JS
-    url: https://github.com/frend/frend.co/blob/gh-pages/_components/dialog-modal/dialog-modal.js
+    url: https://github.com/frend/frend.co/blob/gh-pages/_components/dialogmodal/dialogmodal.js
 links:
   - title: WAI ARIA Authoring Practices - Dialog Modal
     url: https://www.w3.org/TR/wai-aria-practices/#dialog_modal
--- _components/dialogmodal/dialogmodal.js
+++ _components/dialogmodal/dialogmodal.js
@@ -4,13 +4,22 @@
 // https://gist.github.com/paulirish/12fb951a8b893a454b32#gistcomment-1474959
 NodeList.prototype.forEach = Array.prototype.forEach;
 
+// Polyfill matches as per https://github.com/jonathantneal/closest
+Element.prototype.matches = Element.prototype.matches ||
+							Element.prototype.mozMatchesSelector ||
+							Element.prototype.msMatchesSelector ||
+							Element.prototype.oMatchesSelector ||
+							Element.prototype.webkitMatchesSelector;
+
 /**
  * @param {object} options Object containing configuration overrides
  */
 const Frdialogmodal = function ({
 		selector: selector = '.js-fr-dialogmodal',
-		openSelector: openSelector = '.je-fr-dialogmodal-open',
-		readyClass: readyClass = 'fr-dialogmodal--is-ready'
+		openSelector: openSelector = '.js-fr-dialogmodal-open',
+		closeSelector: closeSelector = '.js-fr-dialogmodal-close',
+		readyClass: readyClass = 'fr-dialogmodal--is-ready',
+		activeClass: activeClass = 'fr-dialogmodal--is-active'
 	} = {}) {
 
 
@@ -27,23 +36,106 @@ const Frdialogmodal = function ({
 	// set accordion element NodeLists
 	let modals = doc.querySelectorAll(selector);
 
+	//	TEMP
+	let currButtonOpen = null;
+	let currModal = null;
+
+
+	//	UTILS
+	function _defer (fn) {
+		//	wrapped in setTimeout to delay binding until previous rendering has completed
+		if (typeof fn === 'function') setTimeout(fn, 0);
+	}
+	// function _closest (el, selector) {
+	// 	while (el) {
+	// 		if (el.matches(selector)) break;
+	// 		el = el.parentElement;
+	// 	}
+	// 	return el;
+	// }
+
+
 	// A11Y
 	function _addA11y (modal) {
-
-		let modalId = modal.getAttribute('id');
-		let modalButtonOpen = doc.querySelectorAll(`${openSelector}[aria-controls="${modalId}"]`);
-
-		// add relevant roles and properties
+		//	add relevant roles and properties
 		modal.setAttribute('role', 'dialog');
+		modal.setAttribute('aria-hidden', true);
+	}
+
+
+	//	ACTIONS
+	function _showModal (modal) {
+		//	remove aria-hidden, add focus
+		modal.setAttribute('aria-hidden', false);
+		modal.setAttribute('tabindex', -1);
+		modal.focus();
+		//	sort out events
+		_defer(_bindClosePointer);
+		//	reset scroll position
+		modal.scrollTop = 0;
+		//	add active class
+		modal.classList.add(activeClass);
+	}
+	function _hideModal (modal = currModal, returnfocus = true) {
+		//	add aria-hidden, remove focus
+		modal.setAttribute('aria-hidden', true);
+		modal.removeAttribute('tabindex');
+		modal.blur();
+		//	sort out events
+		_unbindClosePointer(modal);
+		//	remove active class
+		modal.classList.remove(activeClass);
+		//	return focus to button that opened the modal and reset the reference
+		if (returnfocus) {
+			currButtonOpen.focus();
+			currButtonOpen = null;
+		}
+	}
+
+
+	//	EVENTS
+	function _eventOpenPointer (e) {
+		//	get modal
+		let modalId = e.target.getAttribute('aria-controls');
+		let modal = doc.querySelector(`#${modalId}`);
+		//	save temp reference
+		currButtonOpen = e.target;
+		currModal = modal;
+		//	show modal
+		_showModal(modal);
+	}
+	function _eventClosePointer () {
+		_hideModal();
+	}
+
+
+	//	BIND EVENTS
+	function _bindOpenPointers (modal) {
+		const modalId = modal.getAttribute('id');
+		const openButtons = doc.querySelectorAll(`${openSelector}[aria-controls="${modalId}"]`); // is this selector totally crazy?
+		openButtons.forEach((button) => button.addEventListener('click', _eventOpenPointer));
+	}
+	function _bindClosePointer (modal = currModal) {
+		var closeButton = modal.querySelector(closeSelector);
+		closeButton.addEventListener('click', _eventClosePointer);
+	}
+
+
+	//	UNBIND EVENTS
+	function _unbindClosePointer (modal = currModal) {
+		var closeButton = modal.querySelector(closeSelector);
+		closeButton.removeEventListener('click', _eventClosePointer);
 	}
 
 
 	// INIT
 	function init () {
-		if (!modals.length)  return;
+		//	cancel if no modals found
+		if (!modals.length) return;
+		//	loop through available modals
 		modals.forEach((modal) => {
 			_addA11y(modal);
-			_bindClickEvent(modal);
+			_bindOpenPointers(modal);
 			// set ready style hook
 			modal.classList.add(readyClass);
 		});
@@ -53,8 +145,8 @@ const Frdialogmodal = function ({
 
 	// REVEAL API
 	return {
-		init,
-		destroy
+		init
+		// destroy
 	}
 
 }
