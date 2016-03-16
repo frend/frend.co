1f7dbdb375e4ad17c41613fcd4f9a91bed5138c8
Thomas Digby <thomasdigby1@gmail.com>
2016-03-16 11:36:57 +0000

    Updated spread to prototype forEach

--- _components/bypasslinks/bypasslinks.js
+++ _components/bypasslinks/bypasslinks.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// Set Array prototype on NodeList for forEach() support
+// https://gist.github.com/paulirish/12fb951a8b893a454b32#gistcomment-1474959
+NodeList.prototype.forEach = Array.prototype.forEach;
+
 /**
  * @param {object} options Object containing configuration overrides
  */
@@ -42,7 +46,7 @@ const Frbypasslinks = function({
 	}
 	function destroy () {
 		//	loop through each bypass link and remove event bindings
-		[...links].forEach((link) => {
+		links.forEach(link => {
 			_unbindPointerClick(link)
 			_addFocusability(link);
 		});
@@ -99,7 +103,7 @@ const Frbypasslinks = function({
 	function init () {
 		if (!links) return;
 		//	loop through each bypass link
-		[...links].forEach((link) => {
+		links.forEach(link => {
 			_bindPointerClick(link);
 			_removeFocusability(link);
 		});
