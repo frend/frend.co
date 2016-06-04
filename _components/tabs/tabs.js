3288e725ee9c284137ea735a66ba56a5798c4e48
Dan White <danwhite85@gmail.com>
2016-06-03 17:04:01 -0700

    Add closest polyfill for tabs component

--- _components/tabs/tabs.js
+++ _components/tabs/tabs.js
@@ -1,5 +1,36 @@
 'use strict';
 
+// element-closest | CC0-1.0 | github.com/jonathantneal/closest
+if (typeof Element.prototype.matches !== 'function') {
+    Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.webkitMatchesSelector || function matches(selector) {
+        var element = this;
+        var elements = (element.document || element.ownerDocument).querySelectorAll(selector);
+        var index = 0;
+
+        while (elements[index] && elements[index] !== element) {
+            ++index;
+        }
+
+        return Boolean(elements[index]);
+    };
+}
+
+if (typeof Element.prototype.closest !== 'function') {
+    Element.prototype.closest = function closest(selector) {
+        var element = this;
+
+        while (element && element.nodeType === 1) {
+            if (element.matches(selector)) {
+                return element;
+            }
+
+            element = element.parentNode;
+        }
+
+        return null;
+    };
+}
+
 // Set Array prototype on NodeList for forEach() support
 // https://gist.github.com/paulirish/12fb951a8b893a454b32#gistcomment-1474959
 NodeList.prototype.forEach = Array.prototype.forEach;
